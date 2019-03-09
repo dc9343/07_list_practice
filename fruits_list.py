4cc89515d0d664981c9816fa74cd259e72332d43
DC <just-one@hotmail.co.jp>
2019-03-09 16:09:18 +0900

    for文の勉強

---
+++ fruits_list.py
@@ -0,0 +1,8 @@
+fruits = ["Apple", "Banana", "Orange", "Grape"]
+
+# 1分課題:_forを使って、Appleから順に出力してください
+
+for fruit in fruits:
+    print(fruit)
+
+# Q. forを使うと何が嬉しいの? →修正箇所が少なくなる
--- string_list.py
+++ string_list.py
@@ -1,7 +1,12 @@
 members = ["Bob", "Tom", "Ken"]
 
+print(members[0])
+print(members[1])
 print(members[2])
 
-print(members[1])
+# 1分課題:上記の3行分のコードを　for を使って書き直せ
 
-print(members[0])
+# for member in members:
+#     print(members[member[]])
+for member in members:
+    print(member)
